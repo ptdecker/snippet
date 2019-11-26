e933ce0c289b300598ea7c4a3215aa842869479b
Todd Decker <todddecker@Todds-ShotTracker-MacBook-Pro.local>
2019-11-26 11:07:06 -0600

    added custom log handling

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 // Config retains passed command-line flags
@@ -24,6 +25,10 @@ func init() {
 
 func main() {
 
+	// Set-up leveled logging
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	// Initialize new server mux
 	mux := http.NewServeMux()
 
@@ -36,7 +41,14 @@ func main() {
 	fileServer := http.FileServer(http.Dir(cfg.StaticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+	// Set up our new http.Server leveraging our leveled logging
+	srv := &http.Server{
+		Addr:     cfg.Addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
 	// Launch server
-	log.Printf("Starting server on %s\n", cfg.Addr)
-	log.Fatal(http.ListenAndServe(cfg.Addr, mux))
+	infoLog.Printf("Starting server on %s\n", cfg.Addr)
+	errorLog.Fatal(srv.ListenAndServe())
 }
