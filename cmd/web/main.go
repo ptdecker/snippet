82f1ad9b5f1609674bb3823c925663fc658737aa
Todd Decker <todddecker@Todds-ShotTracker-MacBook-Pro.local>
2019-11-26 16:48:10 -0600

    set up database connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,16 +5,20 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // Config retains passed command-line flags
 type config struct {
 	addr      string
 	staticDir string
+	dsn       string
 }
 
 // Application struct is used for application-wide dependencies
@@ -30,6 +34,7 @@ func init() {
 	cfg = new(config)
 	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
 	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.StringVar(&cfg.dsn, "dsn", "web:snippet@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 }
 
@@ -38,6 +43,13 @@ func main() {
 	// Set-up leveled logging
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Initialize database connection pool (MySQL)
+	db, err := openDB(cfg.dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	// Initialize application dependencies
 	app := &application{
@@ -55,4 +67,16 @@ func main() {
 	// Launch server
 	infoLog.Printf("Starting server on %s\n", cfg.addr)
 	errorLog.Fatal(srv.ListenAndServe())
+}
+
+// openDB is a wrapper for sql.Open()
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
