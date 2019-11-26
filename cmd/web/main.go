9e5232a9c29b1621fd833f0e689e314308f0d616
Todd Decker <todddecker@Todds-ShotTracker-MacBook-Pro.local>
2019-11-26 10:53:05 -0600

    Added support for command line parameters

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
+
+// Config retains passed command-line flags
+type Config struct {
+	Addr      string
+	StaticDir string
+}
+
+var cfg *Config
+
+func init() {
+	// Retrieve command-line parameters
+	cfg = new(Config)
+	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.StaticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.Parse()
+}
 
 func main() {
 
@@ -16,10 +33,10 @@ func main() {
 	mux.HandleFunc("/snippet/create", createSnippet)
 
 	// Create a file server to serve static content
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.StaticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	// Launch server
-	log.Println("Starting server on :4000")
-	log.Fatal(http.ListenAndServe(":4000", mux))
+	log.Printf("Starting server on %s\n", cfg.Addr)
+	log.Fatal(http.ListenAndServe(cfg.Addr, mux))
 }
