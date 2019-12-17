448616f7375778fa4dccc9ef77a4854550533ad0
Todd Decker <todddecker@Todds-ShotTracker-MacBook-Pro.local>
2019-12-17 14:42:24 -0600

    Test: Added middleware testing framework

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -28,7 +28,7 @@ import (
 // c.f. https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("X-XSS-Protection", "1; mode-block")
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
 		w.Header().Set("X-Frame-Options", "deny")
 		next.ServeHTTP(w, r)
 	})
